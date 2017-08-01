1c052732a28de28a6542fbb36759343400935c8c
Graeme Bradbury <graeme.bradbury@Siliconshark.com>
2017-08-01 07:26:13 +0100

    Gotten used to function definition

---
+++ Functional Scala/notes/Chapter One/Notes.md
@@ -0,0 +1,3 @@
+#REPL
+
+:load <- command to get the REPL to interprete a scala source file.
--- Functional Scala/src/MyModules.scala
+++ Functional Scala/src/MyModules.scala
@@ -8,6 +8,41 @@ object MyModule {
         msg.format(x, abs(x))
     }
 
-    def main(args: Array[String]): Unit = 
+    def main(args: Array[String]): Unit = {
         println(formatAbs(-42))
+        println(formatFactorial(6))
+        println(formatFibonacci(6))
+    }
+
+    def factorial(n: Int): Int = {
+        @annotation.tailrec
+        def go (n: Int, acc: Int) : Int =
+            if (n <= 0) acc
+            else go (n-1, n*acc)
+
+        go(n, 1)
+    }
+
+    private def formatFactorial(x: Int) = {
+        val msg = "The factorial of %d is %d"
+        msg.format(x, factorial(x))
+    }
+
+    def fibonacci(x: Int) : Int = {
+        if (x == 0) return 0
+        if (x == 1) return 0
+        if (x == 2) return 1
+        @annotation.tailrec
+        def go(count: Int, p: Int, pp: Int) : Int = {
+            if (count <= 0 ) p
+            else go (count -1, p +pp, p)
+        }
+
+        go(x-2, 1, 0)
+    }
+
+    private def formatFibonacci(x: Int) = {
+        val msg = "The %dth fibonacci is %d"
+        msg.format(x, fibonacci(x))
+    }
 }
