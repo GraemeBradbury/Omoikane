f2399b143ac5d6e0a126d8bfaa04b2bb3f3adc8c
Graeme Bradbury <graeme.bradbury@Siliconshark.com>
2017-08-01 07:50:31 +0100

    paramaetric polymorphism

--- Functional Scala/src/MyModules.scala
+++ Functional Scala/src/MyModules.scala
@@ -1,39 +1,25 @@
 object MyModule {
-    def abs (n: Int): Int =
-        if (n < 0) -n
-        else n
-
 
     def main(args: Array[String]): Unit = {
-        println(formatResult("absolute value", -42, abs))
-        println(formatResult("factorial", 7, factorial))
-        println(formatResult("fibonnaci", 5, fibonacci))
+        val order = Array(1,2,3,4,5)
+        var disorder = Array(2,1,3,5,4,7,6)
+        def ordered (a :Int, b: Int): Boolean =
+            b >= a
+        
+        println(isSorted(order, ordered))
+        println(isSorted(disorder, ordered))
     }
 
-    def factorial(n: Int): Int = {
-        @annotation.tailrec
-        def go (n: Int, acc: Int) : Int =
-            if (n <= 0) acc
-            else go (n-1, n*acc)
+    def isSorted[A] (as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+        if (as.length <= 1) true
+        else {
+            @annotation.tailrec
+            def loop(n: Int): Boolean = 
+                if (n >= as.length) true
+                else if (!ordered(as(n-1), as(n))) false
+                else loop(n+1)
 
-        go(n, 1)
-    }
-
-    def fibonacci(x: Int) : Int = {
-        if (x == 0) return 0
-        if (x == 1) return 0
-        if (x == 2) return 1
-        @annotation.tailrec
-        def go(count: Int, p: Int, pp: Int) : Int = {
-            if (count <= 0 ) p
-            else go (count -1, p +pp, p)
+            loop(1)
         }
-
-        go(x-2, 1, 0)
-    }
-
-    private def formatResult(name: String, n: Int, f: Int => Int) = {
-        val msg = "The %s of %d is %d"
-        msg.format(name, n, f(n))
     }
 }
