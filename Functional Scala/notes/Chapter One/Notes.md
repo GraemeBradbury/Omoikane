32e3476fe6e3b87dc2e2dee0530822872a06d72f
Graeme Bradbury <graeme.bradbury@Siliconshark.com>
2017-08-01 18:26:58 +0100

    HOF

--- Functional Scala/notes/Chapter One/Notes.md
+++ Functional Scala/notes/Chapter One/Notes.md
@@ -1,3 +1,10 @@
 #REPL
 
 :load <- command to get the REPL to interprete a scala source file.
+
+
+standard library
+    compose
+    andThen
+
+Page 50
--- Functional Scala/src/MyModules.scala
+++ Functional Scala/src/MyModules.scala
@@ -22,4 +22,11 @@ object MyModule {
             loop(1)
         }
     }
+
+    def uncurry[A,B,C] (f: A => B => C) : (A, B) => C = {
+        (a: A, b: B) => f(a)(b)
+    }
+
+    def compose[A,B,C] (f: B=>C, g: A=>B ): A => C = 
+        (a: A) => f(g(a))
 }
