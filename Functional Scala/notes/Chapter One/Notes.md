64b5eb67ac1e7ead56c73a8660f29c5fdd8dc7c8
Graeme Bradbury <graeme.bradbury@Siliconshark.com>
2017-08-04 08:07:36 +0100

    functional structures

--- Functional Scala/notes/Chapter One/Notes.md
+++ Functional Scala/notes/Chapter One/Notes.md
@@ -7,4 +7,14 @@ standard library
     compose
     andThen
 
-Page 50
+case class Cons...
+    case class is data constructor
+    Cons is traditionally short for construct
+
+case class Cons[+A]
+    ploymorphic in type it contains 'A'
+    + means covariant
+
+def apply[A] (as: A*): List[A]
+    * means variadic function. means zero or more arguments.
+page 52
--- Functional Scala/src/MyModules.scala
+++ Functional Scala/src/MyModules.scala
@@ -1,32 +1,14 @@
+import fpinscala.datastructures
+
 object MyModule {
 
     def main(args: Array[String]): Unit = {
-        val order = Array(1,2,3,4,5)
-        var disorder = Array(2,1,3,5,4,7,6)
-        def ordered (a :Int, b: Int): Boolean =
-            b >= a
-        
-        println(isSorted(order, ordered))
-        println(isSorted(disorder, ordered))
-    }
-
-    def isSorted[A] (as: Array[A], ordered: (A, A) => Boolean): Boolean = {
-        if (as.length <= 1) true
-        else {
-            @annotation.tailrec
-            def loop(n: Int): Boolean = 
-                if (n >= as.length) true
-                else if (!ordered(as(n-1), as(n))) false
-                else loop(n+1)
-
-            loop(1)
+        val x = List(1,2,3,4,5) match {
+            case Cons(x, Cons(2, Cons(4, _))) => x
+            case Nil => 42
+            case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+            case Cons(h, t) => h + sum(t)
+            case _ => 101
         }
     }
-
-    def uncurry[A,B,C] (f: A => B => C) : (A, B) => C = {
-        (a: A, b: B) => f(a)(b)
-    }
-
-    def compose[A,B,C] (f: B=>C, g: A=>B ): A => C = 
-        (a: A) => f(g(a))
 }
