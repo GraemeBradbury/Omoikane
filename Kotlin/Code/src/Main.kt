b889b37d2cf7bee7d86413ea84680dafd66c9f0d
Graeme Bradbury <graeme.bradbury@Siliconshark.com>
2017-08-05 13:30:58 +0100

    Iteratign Maps

--- Kotlin/Code/src/Main.kt
+++ Kotlin/Code/src/Main.kt
@@ -1,5 +1,14 @@
-import geometry.createRandomRectangle
+import java.util.*
 
 fun main(args: Array<String>) {
-    println(eval (Sum(Sum(Num(1), Num(2)), Num(4))))
+    val binaryReps = TreeMap<Char, String>()
+
+    for (c in 'A'..'F') {
+        val binary = Integer.toBinaryString(c.toInt())
+        binaryReps[c] = binary
+    }
+
+    for((letter, binary) in binaryReps) {
+        println("$letter = $binary")
+    }
 }
